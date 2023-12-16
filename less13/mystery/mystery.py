ff7f17448f4d8a6af4af1b7cc109591cd47f0f2c
dimsan <xlxdimsanxlx@gmail.com>
2023-12-16 18:31:22 +0300

    feat: homework 2 lesson 13

---
+++ less13/mystery/mystery.py
@@ -0,0 +1,9 @@
+
+class Question:
+
+    def __init__(self, question, answer, list_wrong_choices):
+        self.question = question
+        self.answer = answer
+        self.list_wrong_choices = list_wrong_choices
+
+        
